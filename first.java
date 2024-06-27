d3bd8774af8a9d1d30e6bdc2e799568b13298f15
sirajudeen I <inayathullahbasha79@gmail.com>
2024-06-27 12:30:29 +0530

    2

--- first.java
+++ first.java
@@ -1,7 +1,7 @@
 
 public class first{
     public static void mainz(String[] args){
-        System.out.println("hello, World!");
+        System.out.println("chikom");
         System.out.println("Namaste world!");
         System.out.println("second branch");
     }
