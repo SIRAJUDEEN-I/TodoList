abdbc7779c23e4d8adf23572b65dba9ff06967e4
sirajudeen I <inayathullahbasha79@gmail.com>
2024-07-21 18:56:35 +0530

    Todolist

--- first.java
+++
@@ -1,9 +0,0 @@
-
-public class first{
-    public static void mainz(String[] args){
-        System.out.println("dandanaka");
-        System.out.println("chikom");
-        System.out.println("Namaste world!");
-        System.out.println("second branch");
-    }
-}
